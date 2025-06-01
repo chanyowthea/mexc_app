063a8d046031331553f03d029617580b9bcd8149
Chanyow Thea <454982165@qq.com>
2025-06-01 15:16:48 +0800

    222

--- mexc_main.py
+++ mexc_main.py
@@ -7,14 +7,17 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 BASE_URL = 'https://api.mexc.com'
 
 
-
-import mexc_spot_v3
-
-
 # import os
 # proxy = 'http://127.0.0.1:7890' # 代理设置，此处修改
 # os.environ['HTTP_PROXY'] = proxy 
 # os.environ['HTTPS_PROXY'] = proxy 
+
+
+
+import mexc_spot_v3
+
+
+
 if __name__ == '__main__':
     trade = mexc_spot_v3.mexc_trade()
     market = mexc_spot_v3.mexc_market()
@@ -25,41 +28,41 @@ if __name__ == '__main__':
     "interval": "1m",
     "limit": "5",
     }
-    quantity = 0.00001
+    quantity_ = 0.00001
 
-    count = 0
+    # count = 0
+    # while True:
+    #     Kline = market.get_kline(params_kline)
+    #     print(f"Kline data: {Kline}", flush=True)
+    #     if count % 2 == 0:
+    #         params = {
+    #             "symbol": symbol,
+    #             "side": "BUY",
+    #             "type": "MARKET",  # Default order type
+    #             "price": float(Kline[-1][4]),  # Use the closing price of the last candle
+    #             "quantity": quantity,
+    #             # 'quoteOrderQty': quoteOrderQty_,
+    #         }
+    #     else:
+    #         params = {
+    #             "symbol": symbol,
+    #             "side": "SELL",
+    #             "type": "MARKET",  # Default order type
+    #             "price": float(Kline[-1][4]),  # Use the closing price of the last candle
+    #             "quantity": quantity,
+    #             # 'quoteOrderQty': quoteOrderQty_,
+    #         }
+    
+    #     print(f"place_order params: {params}", flush=True)
+    #     PlaceOrder = trade.post_order(params)
+    #     print(f"Order result: {PlaceOrder}", flush=True)
+    #     sleep(1)
+    #     count += 1
+
+    has_buy = False
     while True:
         Kline = market.get_kline(params_kline)
-        print(f"Kline data: {Kline}", flush=True)
-        if count % 2 == 0:
-            params = {
-                "symbol": symbol,
-                "side": "BUY",
-                "type": "MARKET",  # Default order type
-                "price": float(Kline[-1][4]),  # Use the closing price of the last candle
-                "quantity": quantity,
-                # 'quoteOrderQty': quoteOrderQty_,
-            }
-        else:
-            params = {
-                "symbol": symbol,
-                "side": "SELL",
-                "type": "MARKET",  # Default order type
-                "price": float(Kline[-1][4]),  # Use the closing price of the last candle
-                "quantity": quantity,
-                # 'quoteOrderQty': quoteOrderQty_,
-            }
-    
-        print(f"place_order params: {params}", flush=True)
-        PlaceOrder = trade.post_order(params)
-        print(f"Order result: {PlaceOrder}", flush=True)
-        sleep(1)
-        count += 1
-
-
-    while False:
-        Kline = market.get_kline(params)
-        print(f"Kline data: {Kline}", flush=True)
+        # print(f"Kline data: {Kline}", flush=True)
         if not Kline or len(Kline) <= 2:
             print("====Kline data is not sufficient or empty.", flush=True)
             sleep(1)
@@ -70,7 +73,7 @@ if __name__ == '__main__':
         prev_k_l = prev_k[3]
         cur_k_c = Kline[-1][4]
 
-        print('prev_k=',prev_k,prev_k_h, prev_k_l, cur_k_c,flush=True)
+        # print('prev_k=',prev_k,prev_k_h, prev_k_l, cur_k_c,flush=True)
         Price = cur_k_c
         order_type = 'MARKET'  # Default order type
         side = None
@@ -82,7 +85,15 @@ if __name__ == '__main__':
             side = 'SELL'
 
         if side is not None:
-            quantity = 0.0005
+            has_buy_temp = True if side == 'BUY' else False
+            if has_buy_temp == has_buy:
+                # print(f"====No change in position, skipping order. Current side: {side}, Previous side: {has_buy}", flush=True)
+                sleep(1)
+                continue
+            else:
+                has_buy = has_buy_temp
+
+            quantity = quantity_
             params = {
                 "symbol": symbol,
                 "side": side,
